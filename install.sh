6b836ed055a12b735855a68fa45e27aa1c1579cf
CybeSecurityOficial <82549779+CybeSecurityOficial@users.noreply.github.com>
2021-04-14 21:25:40 -0300

    IpFinder

---
+++ install.sh
@@ -0,0 +1,12 @@
+clear
+
+printf 'Instalando modulos'
+
+apt install python python2 -y
+apt install python3 -y
+apt install python3-pip -y
+pip3 install requests
+clear
+
+printf 'Agora basta rodar -> python3 main.py\n'
+printf '_Cyber Security_ '
